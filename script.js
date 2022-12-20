8d65e0a9f3961219d46040365953a456e6eb4b87
laravell2004 <121086089+laravell2004@users.noreply.github.com>
2022-12-20 14:48:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const buttonMulai = document.querySelector('.button-modal');
+buttonMulai.addEventListener('click', function() {
+    fetch('https://candaan-api.vercel.app/api/text/random')
+    .then(response => response.json())
+    .then(response => {
+        const modalBody = document.querySelector('.modal-body')
+        modalBody.innerHTML = showDetail(response);
+    })
+})
+
+
+
+
+
+
+function showDetail(m) {
+    return `<div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="assets/profil.png" class="img-fluid">
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h4>Semoga Ngakak</h4></li>
+                            <li class="list-group-item"><strong>Jokes : ${m.data}</li>
+        
+                        </ul>
+                    </div>
+                </div>
+            </div>`
+}
